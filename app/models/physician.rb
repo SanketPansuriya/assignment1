89852d75a0412634938a4b6bc8e9e942b60305fa
SanketPansuriya <sanket.panasuriya@bacancy.com>
2022-05-09 16:31:04 +0530

    email validation added from server side

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -2,7 +2,11 @@ class Physician < ApplicationRecord
 	before_create :generate_unique_id
 	
 	validates :name, :email, presence: true
-	validates :email, :unique_id, uniqueness: true
+	validates :unique_id, uniqueness: true
+	validates :email, format: { with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/, 
+				message: " invalid"  }, 
+				uniqueness: { case_sensitive: false }, 
+				length: { minimum: 4, maximum: 254 }
 
 	has_many :appointments
 	has_many :patients, through: :appointments
